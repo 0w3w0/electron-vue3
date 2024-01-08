756cdc6886a3b378107f2c3606f249068ae9da16
0w3w0 <41020733@qq.com>
2024-01-08 20:07:11 +0800

    refactor: example

---
+++ src/renderer/components/Button.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button class="app-button">
+    <slot></slot>
+  </button>
+</template>
+
+<style lang="scss">
+@use "@/renderer/styles/components/button";
+</style>
